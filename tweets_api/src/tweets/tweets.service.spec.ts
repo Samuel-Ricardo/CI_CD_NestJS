aa399ce4051a3bcd8f918d7f2c1eb8ad96fbbe4c
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-16 16:36:48 -0300

    [:white_check_mark:]  | create: test suit - service => Tweet (module)

--- tweets_api/src/tweets/tweets.service.spec.ts
+++ tweets_api/src/tweets/tweets.service.spec.ts
@@ -1,15 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TweetsService } from './tweets.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Tweet, TweetSchema } from './entities/tweet.entity';
 
 describe('TweetsService', () => {
   let service: TweetsService;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot(
+          'mongodb://root:root@localhost:27017/tweets_service_test?authSource=admin',
+        ),
+        MongooseModule.forFeature([{ name: Tweet.name, schema: TweetSchema }]),
+      ],
       providers: [TweetsService],
     }).compile();
 
     service = module.get<TweetsService>(TweetsService);
+  });
+
+  afterEach(async () => {
+    await module.close();
   });
 
   it('should be defined', () => {
