3644b3e1eb5ff9822f6f82fea6ca01071b39591a
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-16 16:37:08 -0300

    [:white_check_mark:]  | create: test suit - controller => Tweet (module)

--- tweets_api/src/tweets/tweets.controller.spec.ts
+++ tweets_api/src/tweets/tweets.controller.spec.ts
@@ -1,17 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TweetsController } from './tweets.controller';
 import { TweetsService } from './tweets.service';
+import { Tweet, TweetSchema } from './entities/tweet.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 describe('TweetsController', () => {
   let controller: TweetsController;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot(
+          'mongodb://root:root@localhost:27017/tweets_controller_test?authSource=admin',
+        ),
+        MongooseModule.forFeature([{ name: Tweet.name, schema: TweetSchema }]),
+      ],
       controllers: [TweetsController],
       providers: [TweetsService],
     }).compile();
 
     controller = module.get<TweetsController>(TweetsController);
+  });
+
+  afterEach(async () => {
+    await module.close();
   });
 
   it('should be defined', () => {
