dbae19208d0dbe698fda59fc87ddacc180069673
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-16 15:36:20 -0300

    [:card_file_box:]  | setup: db (docker)

--- tweets_api/src/tweets/entities/tweet.entity.spec.ts
+++ tweets_api/src/tweets/entities/tweet.entity.spec.ts
@@ -1,4 +1,8 @@
-import { Tweet } from './tweet.entity';
+import mongoose from 'mongoose';
+import { Tweet, TweetSchema } from './tweet.entity';
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 describe('[ENTITY] | Tweets', () => {
   describe('[ENTITY] | Tweets (Unit Test)', () => {
@@ -10,6 +14,35 @@ describe('[ENTITY] | Tweets', () => {
 
       expect(tweet.content).toBe('Hello World');
       expect(tweet.screen_name).toBe('Luiz Carlos');
+    });
+  });
+
+  describe('[ENTITY] | Tweets (Integration Test)', () => {
+    let conn: mongoose.Mongoose;
+
+    jest.setTimeout(30000);
+
+    beforeEach(async () => {
+      // const app: TestingModule = await Test.createTestingModule({
+      //   imports: [
+      //     ConfigModule.forRoot({envFilePath: '.env.test'}),
+      //     MongooseModule.forRoot(process.env.MONGO_ENTITY_TEST)
+      //   ]
+      // }).compile();
+
+      // conn = await app.get();
+
+      conn = await mongoose.connect(
+        `mongodb://root:root@localhost:27017/tweets_entity_test?authSource=admin`,
+      );
+    });
+
+    afterEach(async () => {
+      await conn.connection.close();
+    });
+
+    it('Should Create a Tweet', async () => {
+      expect(true).toBe(true);
     });
   });
 });
