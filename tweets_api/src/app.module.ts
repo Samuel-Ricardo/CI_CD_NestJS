3e2173dd77b244ba02181df3cc6275e84cac4b00
samuel-ricardo <samuelricardoofficial@gmail.com>
2023-07-16 12:36:34 -0300

    [:gear:] | setup: module (app)

--- tweets_api/src/app.module.ts
+++ tweets_api/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        join(__dirname, '..', `.env.${process.env.NODE_ENV}`),
+        join(__dirname, '..', `.env`),
+      ],
+    }),
+    MongooseModule.forRoot(process.env.MONGO_DSN),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
